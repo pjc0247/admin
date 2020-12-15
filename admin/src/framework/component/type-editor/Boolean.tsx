9769a9308cdd3e34c5c16bb83ed380221cfb0bd4
pjc0247 <pjc0247@naver.com>
2020-12-15 21:54:03 +0900

    update

--- admin/src/framework/component/type-editor/Boolean.tsx
+++ admin/src/framework/component/type-editor/Boolean.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import { FormControlLabel, Switch } from '@material-ui/core';
+import { useFormikContext } from 'formik';
 
 type BooleanProps = {
+  name: string;
   label: string;
   value: boolean;
-  onChange: (v: boolean) => void;
+  onChange: (v: any) => void;
 };
 export const Boolean = ({
+  name,
   label,
   value,
   onChange,
   ...props
 }: BooleanProps) => {
+  const {
+    setFieldValue,
+  } = useFormikContext();
+  
   return (
     <FormControlLabel
       label={label}
       control={(
         <Switch
           checked={value}
-          onChange={(e: any) => console.log(e)}
+          onChange={(e) => setFieldValue(name, e.target.checked)}
         />
       )}
     />
