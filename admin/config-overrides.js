248014e2dcb774144e3be39ce46420efa317818e
Park June Chul <pjc0247@naver.com>
2021-12-07 00:04:42 +0900

    Update config-overrides.js

--- admin/config-overrides.js
+++ admin/config-overrides.js
@@ -4,6 +4,14 @@ module.exports = function override(config) {
   config.resolve = {
     ...config.resolve,
   };
-
+  config.optimization = {
+    ...config.optimization,
+    minimize: false,
+    splitChunks: {
+        chunks: 'all',
+        name: true
+    },
+    runtimeChunk: true,
+  };
   return config;
 };
